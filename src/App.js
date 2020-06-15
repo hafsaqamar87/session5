1c85a77a82767d9cf9057d536195d8c46d6993c0
hafsaqamar87 <60553705+hafsaqamar87@users.noreply.github.com>
2020-06-15 22:39:05 +0500

    Initialize project using Create React App

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, {useState} from 'react';
+import './App.css';
+import Parent from './Parent';
+import counterContext from './CounterContext';
+
+
+function App() {
+  let countState=useState(1) //[count, SetCount]
+  return (
+    <counterContext.Provider value={countState}>
+    <div>
+      <Parent />
+    </div>
+     </counterContext.Provider>
+  );
+}
+
+export default App;
---
+++ src/CounterReducer.js
@@ -0,0 +1,9 @@
+const counterReducer = (state,action) => {
+    switch (action){
+        case 'INCREMENT' :
+            return state + 1;
+        default:
+            return state;
+        }
+}
+export default counterReducer;
